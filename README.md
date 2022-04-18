3daf2411fb966a48b07f07e12c651af2bca78814
Zack <58833995+Zackydoo@users.noreply.github.com>
2022-04-18 14:49:36 +0000

    Better Read Me and fixed some things in the slot machine. The new lottery has still yet to be added in.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Magicalus-Casino-MAIN
 An ASCII-based casino made entirely by me.
-This is my first big project.
-Also, you may notice that a lot of code just appeared in the first commit. Thats because I copied a lot of the code from an older version that had a bad repository.
+
+This is my first big project, and thus I am irrationally proud of it.
+
+There used to be a different version, but I bugged out the repository.
--- main.py
+++ main.py
@@ -9,10 +9,13 @@ def clear():
         system('clear')
 def randgen():
   return time.time()*1000
+def lottonumbers():
+  random.seed(randgen())
+  return random.randint(0,99)
 bal=0
-loan=0
-print("Hello, and welcome to the Magicalus Casino!\nYou've entered with nothing, and need to make your fortune.\nDon't worry: there's no penalty for going into curshing, crushing debt. So have fun, and hit anything to start!")
-hghg=input
+clear()
+print("Hello, and welcome to the Magicalus Casino!\nYou've entered with nothing, and need to make your fortune.\nDon't worry: there's no penalty for going into crushing, crushing debt. So have fun, and hit anything to start!\nNote: Due to lack of coding skill, a win state has not been added yet.")
+hghg=input()
 while True:
   clear()
   print("Welcome to the Magicalus Casino Lobby!\nYou have "+str(bal)+" Magicalus bucks\nPress 1 to play the Lottery\nPress 2 to hit the Slots")
@@ -21,14 +24,26 @@ while True:
     clear()
     playagain=""
     while True:
-     price=10
-     if playagain=="back":
-      break
-     print("You have "+str(bal)+" Magicalus Bucks. It costs "+str(price)+" to play.")
-     print("Press enter to play. Or say back to go to the casino floor.")
-     back=input()
-     if back=="back":
-      break
+      price=10
+      if playagain=="back":
+       break
+      print("You have "+str(bal)+" Magicalus Bucks. It costs "+str(price)+" to play.\nPress enter to play. Or say back to go to the casino floor.")
+      back=input()
+      if back=="back":
+        break
+      num1=lottonumbers()
+      num2=lottonumbers()
+      num3=lottonumbers()
+      num4=lottonumbers()
+      num5=lottonumbers()
+      while True:
+        win1=lottonumbers()
+        win2=lottonumbers()
+        win3=lottonumbers()
+        win4=lottonumbers()
+        win5=lottonumbers()
+        print("Your numbers are:"+str(num1)+" "+str(num2)+" "+str(num3)+" "+str(num4)+" "+str(num5))
+        hghgh=input()
   elif x=="2":
     clear()
     playagain=""
@@ -36,8 +51,8 @@ while True:
       price=10
       if playagain=="back":
         break
-      print("Slot Machine Wing\nYou have "+str(bal)+" Magicalus bucks")
-      print("Press enter to play, or say "+'"back"'+" to go back")
+      print("Welcome to the Slot Floor!\nYou have "+str(bal)+" Magicalus bucks")
+      print('Press enter to play, or say "back" to go back')
       play=input()
       if play=="back":
         break
@@ -46,11 +61,10 @@ while True:
       epoch=math.floor(time.time())
       slottime=math.ceil(epoch)
       runtime=0
-      s1t=random.randint(10,20)*0.1
-      s2t=random.randint(21,30)*0.1
-      s3t=random.randint(31,40)*0.1
-      slotodds=[1,2,3,4,5,6,7,8,9]
-      rigging=[9,8,7,6,5,4,3,2,1]
+      s1t=random.randint(30,50)
+      s2t=random.randint(52,72)
+      s3t=random.randint(74,94)
+      slotodds=[1,1,1,1,1,2,2,2,2,3,3,3,4,4,5]
       slot1=random.randint(1,5)
       slot2=random.randint(1,5)
       slot3=random.randint(1,5)
@@ -60,50 +74,46 @@ while True:
       slot1n=random.randint(1,5)
       slot2n=random.randint(1,5)
       slot3n=random.randint(1,5)
+      winning=0
       while True:
-       epoch=math.floor(time.time())
-       if slottime<epoch:
-         slottime=math.ceil(time.time())
-         runtime=runtime+1 
-       if s1t>runtime:
-         slot1p=slot1
-         slot1=slot1n
-         slot1n=random.choices(population=slotodds, weights=rigging)
-       if s2t>runtime:
-         slot2p=slot2
-         slot2=slot2n
-         slot2n=random.choices(population=slotodds, weights=rigging)
-       if s3t>runtime:
-         slot3p=slot3
-         slot3=slot3n
-         slot3n=random.choices(population=slotodds, weights=rigging)
-       print("╔═════╤═════╤═════╗\n║ "+str(slot1n)+" | "+str(slot2n)+" | "+str(slot3n)+" ║\n║     |     |     ║\n║     |     |     ║\n║ "+str(slot1)+" | "+str(slot2)+" | "+str(slot3)+" ║\n║     |     |     ║\n║     |     |     ║\n║ "+str(slot1p)+" | "+str(slot2p)+" | "+str(slot3p)+" ║\n╚═════╧═════╧═════╝")
-       if s1t>runtime or s2t>runtime or s3t>runtime:
-         clear()
-         continue
-       time.sleep(1.5)
-       clear()
-       if slot1==slot2==slot3 or slot1n==slot2==slot3p or slot1p==slot2==slot3n:
-        paya=str(slot2)
-       elif slot1n==slot2n==slot3n:
-        paya=str(slot2n)
-       elif slot1p==slot2p==slot3p:
-        paya=str(slot2p) 
-       else:
-         paya=0
-       if paya==0:
-         print("Better luck next time!")
-       else:
-        paya=paya.replace("[","")
-        payout=10**int(paya.replace("]",""))
-        bal=bal+payout
-        print("You won "+str(payout)+" Magicalus bucks!")
-       print("Say back to leave, or say anything else to spin the wheels again!")
-       playagain=input()
-       clear()
-       break
+        if s1t>runtime:
+          slot1p=slot1
+          slot1=slot1n
+          slot1n=random.choice(slotodds)
+        if s2t>runtime:
+          slot2p=slot2
+          slot2=slot2n
+          slot2n=random.choice(slotodds)
+        if s3t>runtime:
+          slot3p=slot3
+          slot3=slot3n
+          slot3n=random.choice(slotodds, )
+        print("╔═════╤═════╤═════╗\n║ ["+str(slot1n)+"] | ["+str(slot2n)+"] | ["+str(slot3n)+"] ║\n║     |     |     ║\n║     |     |     ║\n║ ["+str(slot1)+"] | ["+str(slot2)+"] | ["+str(slot3)+"] ║\n║     |     |     ║\n║     |     |     ║\n║ ["+str(slot1p)+"] | ["+str(slot2p)+"] | ["+str(slot3p)+"] ║\n╚═════╧═════╧═════╝")
+        time.sleep(0.075)
+        if s3t>runtime:
+          clear()
+          runtime=runtime+1
+          continue
+        time.sleep(1.5)
+        clear()
+        if slot1==slot2==slot3 or slot1n==slot2==slot3p or slot1p==slot2==slot3n:
+          winning=slot2
+        elif slot1n==slot2n==slot3n:
+          winning=slot2n
+        elif slot1p==slot2p==slot3p:
+          winning=slot2p
+        if winning==0:
+          print("Better luck next time!")
+        else:
+          payout=10**int(winning)
+          bal=bal+payout
+          print("You won "+str(payout)+" Magicalus bucks!")
+        print("Say back to leave, or say anything else to spin the wheels again!")
+        playagain=input()
+        clear()
+        break
   else:
-   clear()
-   print("Theres nothing here. Hit enter to go back.")
-   c=input()
-   continue
+    clear()
+    print("Theres nothing here. Hit enter to go back.")
+    c=input()
+    continue
